cad28c9511fd6b6c19e92ce2d12d778c322c5182
MateoBrun <matebrun@gmail.com>
2023-08-13 22:03:05 -0300

    solucion del click

--- eventos.js
+++ eventos.js
@@ -3,3 +3,8 @@ var ClickDiv = document.getElementById("divs");
 ClickDiv.addEventListener("click", function(){
     alert("Hola, Soy el Div");
 })
+
+boton.addEventListener("click", function(sacarclick) {
+    sacarclick.stopPropagation();
+
+})
